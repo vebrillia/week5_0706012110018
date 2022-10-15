46b8c0a7b45857deb0ab841d0f281f0720e8604a
vebrillia s <vsantoso01@ciputra.ac.id>
2022-10-15 23:43:07 +0700

    initial commit

---
+++ app/src/main/java/com/uc/week4retrofit/helper/Const.kt
@@ -0,0 +1,8 @@
+package com.uc.week4retrofit.helper
+
+object Const {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY ="60484e2fdf96b3155d8c5abc4f0338aa"
+
+    const val IMG_URL = "https://image.tmdb.org/t/p/w500/"
+}
